da865e7e4d6337d357fec7d2ee7ac71c7be7d5a2
sabliao <sabrinagliao@gmail.com>
2017-08-18 00:12:53 -0700

    Progress up to end of Section 2, Lecture 28: embed youtube vid detail

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAt2m8eft3DMp_HMGbzD2C1ISQFz6d-grw';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-    console.log(data);
-});
-
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+            // same as this.setState({ videos: videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
